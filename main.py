492ee820eba5263651806658ce551d1516c7fa86
younes_qasempour <qasempouryounes@gmail.com>
2025-02-19 11:28:29 +0330

    Initial commit

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
-with open("weather_data.csv") as file:
-    content = file.read()
-    print(content)
+# with open("weather_data.csv") as file:
+#     data = file.readlines()
+#     print(data)
+
+# import csv
+#
+# temperatures = []
+# with open("weather_data.csv") as file:
+#     data = csv.reader(file)
+#     for row in data:
+#         temperatures.append(row[1])
+#     temperatures.pop(0)
+#     temperatures = list(map(int, temperatures))
+#     print(temperatures)
+
+import pandas as pd
+
+data = pd.read_csv("weather_data.csv")
+print(data["temp"])
