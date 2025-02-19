93883878fa4ac724cc8aeaa91c3704708f9c2e40
younes_qasempour <qasempouryounes@gmail.com>
2025-02-19 11:38:39 +0330

    Initial commit

--- main.py
+++ main.py
@@ -16,4 +16,17 @@
 import pandas as pd
 
 data = pd.read_csv("weather_data.csv")
-print(data["temp"])
+# print(data["temp"])
+
+data_dict = data.to_dict()
+# print(data_dict)
+
+temp_list = data['temp'].to_list()
+# print(temp_list)
+
+mean = data['temp'].mean()
+# print(mean)
+
+max_value = data['temp'].max()
+# print(max_value)
+
